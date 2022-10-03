634945e2c7a12d6c582ac85a94218495d58f3204
teoff9 <98432403+teoff9@users.noreply.github.com>
2022-10-03 21:37:41 +0200

    pip > pip3

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from os import system
 
 #install the required dependencies
 
-system("pip install --upgrade -r requirements.txt")
+system("pip3 install --upgrade -r requirements.txt")
 print("")
 print("All requirements installed! Proceed on launching MRME.py")
